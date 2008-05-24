659780c19399638e7732e9d83d895cb74318112c
meteorafhd@gmail.com <meteorafhd@gmail.com@d4cc848e-f34d-0410-b80f-25bf1cf6c937>
2008-05-24 06:14:30 +0000

    Initial import.

---
+++ trunk/meteorafhd/h/src/com/entity/Std.java
@@ -0,0 +1,35 @@
+/*
+ * Created Tue May 16 15:56:54 CST 2006 by MyEclipse Hibernate Tool.
+ */ 
+package com.entity;
+
+import java.io.Serializable;
+
+/**
+ * A class that represents a row in the 'std' table. 
+ * This class may be customized as it is never re-generated 
+ * after being created.
+ */
+public class Std
+    extends AbstractStd
+    implements Serializable
+{
+    /**
+     * Simple constructor of Std instances.
+     */
+    public Std()
+    {
+    }
+
+    /**
+     * Constructor of Std instances given a simple primary key.
+     * @param sid
+     */
+    public Std(java.lang.Integer sid)
+    {
+        super(sid);
+    }
+
+    /* Add customized code below */
+
+}
