b3a24df175072bfa02c4223bad78a42c4d58bebe
meteorafhd@gmail.com <meteorafhd@gmail.com@d4cc848e-f34d-0410-b80f-25bf1cf6c937>
2008-05-24 06:48:20 +0000

    Initial import.

---
+++ trunk/meteorafhd/testEJB/src/helloworld/ejb/HelloWorld.java
@@ -0,0 +1,81 @@
+package helloworld.ejb;
+
+import java.rmi.RemoteException;
+
+import javax.ejb.EJBException;
+import javax.ejb.SessionBean;
+import javax.ejb.SessionContext;
+
+
+/**
+ * XDoclet-based session bean.  The class must be declared
+ * public according to the EJB specification.
+ *
+ * To generate the EJB related files to this EJB:
+ *		- Add Standard EJB module to XDoclet project properties
+ *		- Customize XDoclet configuration for your appserver
+ *		- Run XDoclet
+ *
+ * Below are the xdoclet-related tags needed for this EJB.
+ * 
+ * @ejb.bean name="HelloWorld"
+ *           display-name="Name for HelloWorld"
+ *           description="Description for HelloWorld"
+ *           jndi-name="ejb/HelloWorld"
+ *           type="Stateless"
+ *           view-type="remote"
+ */public class HelloWorld implements SessionBean {
+
+	 /** The session context */
+	 private SessionContext context;
+
+	 public String hello() throws EJBException {
+		 // rename and start putting your business logic here
+		 return new String("HelloEJBWorld!");
+	 }
+
+	 @Override
+	 public void ejbActivate() throws EJBException, RemoteException {
+		 // TODO Auto-generated method stub
+
+	 }
+
+	 @Override
+	 public void ejbPassivate() throws EJBException, RemoteException {
+		 // TODO Auto-generated method stub
+
+	 }
+
+	 @Override
+	 public void ejbRemove() throws EJBException, RemoteException {
+		 // TODO Auto-generated method stub
+
+	 }
+
+	 /**
+	  * Set the associated session context. The container calls this method 
+	  * after the instance creation.
+	  * 
+	  * The enterprise bean instance should store the reference to the context 
+	  * object in an instance variable.
+	  * 
+	  * This method is called with no transaction context. 
+	  * 
+	  * @throws EJBException Thrown if method fails due to system-level error.
+	  */
+	 public void setSessionContext(SessionContext newContext) throws EJBException {
+		 context = newContext;
+	 }
+
+	 /**
+	  * An example business method
+	  *
+	  * @ejb.interface-method view-type = "remote"
+	  * 
+	  * @throws EJBException Thrown if method fails due to system-level error.
+	  */
+	 public void replaceWithRealBusinessMethod() throws EJBException {
+//		 rename and start putting your business logic here
+	 }
+
+ }
