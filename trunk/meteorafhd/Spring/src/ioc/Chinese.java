8dabae93d1cb15b5af04528e8c552071f6118e4b
meteorafhd@gmail.com <meteorafhd@gmail.com@d4cc848e-f34d-0410-b80f-25bf1cf6c937>
2008-05-24 08:22:52 +0000

    Initial import.

---
+++ trunk/meteorafhd/Spring/src/ioc/Chinese.java
@@ -0,0 +1,14 @@
+package ioc;
+
+public class Chinese implements Human {
+
+	public void eat() {
+		System.out.println("中国人对吃很有一套");
+	}
+
+	public void walk() {
+		System.out.println("中国人行如飞");
+	}
+
+}
+
---
+++ trunk/meteorafhd/Spring/src/mail/MailTest.java
@@ -0,0 +1,38 @@
+package mail;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class MailTest {
+	private MailSender mailSender;
+	private SimpleMailMessage templateMessage;
+	public MailTest(MailSender mailSender, SimpleMailMessage templateMessage) {
+		super();
+		this.mailSender = mailSender;
+		this.templateMessage = templateMessage;
+	}
+	
+	public void sendMeMail() {
+		SimpleMailMessage msg = new SimpleMailMessage(templateMessage);
+		msg.setTo("meteorafhd@gmail.com");
+		msg.setText("This is a test.\nGo Spring!\n");
+		try {
+			this.mailSender.send(msg);
+		} catch (MailException e) {
+			System.err.println("Didn't work.");
+			e.printStackTrace();
+		}
+	}
+	
+	public static final void main(String[] args) {
+		ApplicationContext appCtx = new ClassPathXmlApplicationContext(new String[] {
+				"mail/mail2.xml"
+		});
+		
+		MailTest tester = (MailTest) appCtx.getBean("mailTest");
+		tester.sendMeMail();
+	}
+}
