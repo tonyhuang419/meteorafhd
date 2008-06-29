f81ba219b8c0f9ecc367aa59d9aef5f28d265c61
meteorafhd <meteorafhd@gmail.com>
2008-06-29 09:27:39 +0000

    Initial import.

---
+++ test/src/ObjectTest.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ObjectTest {
+
+	public void test(Object...x){
+		for(Object obj:x)
+			System.out.print(obj+" ");
+	}
+
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		list.add("e");
+		new ObjectTest().test("a","b","c","d","r");
+
+	}
+}
---
+++ test/src/Test.java
@@ -0,0 +1,28 @@
+
+
+public class Test {
+	public Stu getStu(){
+		return new Stu();
+	}
+	public static void main(String[] args) {
+//		Stu s = null;
+//		try{
+//			System.out.println(s.getClass().getName());
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		s = new Test().getStu();
+//		try{
+//			System.out.println(s.getClass().getName());
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+		
+//		BigDecimal a = new BigDecimal(100.00);
+//		System.out.println(a.add(new BigDecimal(-1)));
+
+		String s="";
+		System.out.println(s.trim());
+	}
+
+}
