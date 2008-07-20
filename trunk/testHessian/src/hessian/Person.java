1011aee322ad32f8263b854b6b4cbe056c1a1fa3
meteorafhd@gmail.com <meteorafhd@gmail.com@d4cc848e-f34d-0410-b80f-25bf1cf6c937>
2008-07-20 15:20:33 +0000

    testHessian

---
+++ trunk/testHessian/src/hessian/Person.java
@@ -0,0 +1,35 @@
+package hessian;
+
+import java.io.Serializable;
+
+public class Person implements Serializable{
+
+	private static final long serialVersionUID = 1407404432178059807L;
+	private String name;  
+	private String length;  
+	private String color;  
+
+	public String getName() {  
+		return name;  
+	}  
+
+	public void setName(String name) {  
+		this.name = name;  
+	}  
+
+	public String getLength() {  
+		return length;  
+	}  
+
+	public void setLength(String length) {  
+		this.length = length;  
+	}  
+
+	public String getColor() {  
+		return color;  
+	}  
+
+	public void setColor(String color) {  
+		this.color = color;  
+	} 
+}
