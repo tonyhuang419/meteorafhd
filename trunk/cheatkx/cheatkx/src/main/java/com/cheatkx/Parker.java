1917bfd3f7ac84a05461a2de2edd73966daf2ef0
meteorafhd@gmail.com <meteorafhd@gmail.com@d4cc848e-f34d-0410-b80f-25bf1cf6c937>
2008-10-11 08:46:14 +0000

    Initial import.

---
+++ trunk/cheatkx/cheatkx/src/main/java/com/cheatkx/Parker.java
@@ -0,0 +1,73 @@
+package com.cheatkx;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Parker {
+	
+	static String urlStr = "www.kaixin001.com";
+	static int port = 80;
+	static String protocal = "http";
+	String g_verify;
+
+	protected Log logger = LogFactory.getLog(this.getClass());
+	
+	public static void main(String[] args) throws IOException
+	{
+		Login login = new Login();
+		Parker p = new Parker();
+		HttpClient client = login.login(urlStr,port,protocal);
+		if( client != null ){	
+			p.parker(client); 
+			p.showUserCar(client);
+		}
+		else{
+//			here can't use logger.info
+//			Cannot make a static reference to the non-static field logger
+//			System.out.println("login fail");
+		}
+//		login.showCookie(urlStr, client);
+		login.logout(client,urlStr,port,protocal);	
+	}
+	
+	
+	private void parker( HttpClient client  ){
+		String methodUrl = "/app/app.php?aid=1040";
+//		client.getHostConfiguration().setHost(urlStr, port, protocal );		
+//		HttpMethod method = getPostMethod(methodUrl);//使用POST方式提交数据
+		HttpMethod method = new GetMethod(methodUrl);
+		try{
+			client.executeMethod(method);
+			String context = Tools.printInfo(method);
+			g_verify = Tools.getVerify(context);			
+			logger.info(Tools.processUserData(context));
+			
+		}catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+	}
+	
+	private void showUserCar( HttpClient client ){
+		String verify = g_verify;
+		String puid = "3327550";
+		String methodUrl = "/parking/usercar.php?verify="+verify+"&puid="+puid;
+		HttpMethod method = new GetMethod(methodUrl);
+		try{
+			client.executeMethod(method);
+//			String context = Tools.printInfo(method);		
+		}
+		catch(IOException ioe){
+			ioe.printStackTrace();
+		}	
+	}
+	
+
+}
